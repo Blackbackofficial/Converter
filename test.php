3c19faf07852acd00d6ba7844dcbf755d5b8101f
Chernov I.E <chernov_ie@otd05.npc.local>
2020-09-30 16:27:34 +0300

    1)Json -> xlsx 2)Clean code

--- test.php
+++ test.php
@@ -16,20 +16,27 @@ $arrayStyleFirstRow = [
 ];
 
 $coordinates = ['A1','B1','C1','D1','E1'];
-$FirstStr = ['Id','ШК','Название','Кол-во','Сумма'];
+$firstStr = ['Id','ШК','Название','Кол-во','Сумма'];
 
 //Bold first text
 foreach ($coordinates as $item) {
     $sheet->getStyle($item)->applyFromArray($arrayStyleFirstRow);
 }
 for($i = 0; $i < count($coordinates); $i++){
-    $sheet->setCellValue($coordinates[$i], $FirstStr[$i]);
+    $sheet->setCellValue($coordinates[$i], $firstStr[$i]);
 }
 
-// Записываем в ячейку A1 данные
-$sheet->setCellValue('A2', 47323424);
-$sheet->setCellValue('B2', 47323424);
+$json = json_decode(file_get_contents('order.json' ), true, 512,JSON_UNESCAPED_UNICODE);
+$json = $json['items'];
 
+// Записываем в ячейки данные
+for($j = 0; $j < count($json); $j++) {
+    $sheet->setCellValue('A'.($j+2), $json[$j]['id']);
+    $sheet->setCellValue('B'.($j+2), '000000'.$json[$j]['item']['external_code']);
+    $sheet->setCellValue('C'.($j+2), $json[$j]['item']['name']);
+    $sheet->setCellValue('D'.($j+2), $json[$j]['quantity']);
+    $sheet->setCellValue('E'.($j+2), $json[$j]['amount']);
+}
 try {
     $writer = new Xlsx($spreadsheet);
     $writer->save('hello.xlsx');
